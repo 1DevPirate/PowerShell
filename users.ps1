05ed6c4b58c92b80c4b172d91cbd9492486d8dc1
choncey <nchoncey@gmail.com>
2023-12-05 13:15:35 -0800

    created user script

--- users.ps1
+++ users.ps1
@@ -1,2 +1,2 @@
-# This will create new users
+# This will create a new user: Choncey
 New-ADUser -Name "Choncey Nunn" -GivenName "Choncey" -Surname "Nunn" -SamAccountName "choncey" AccountPassword (ConvertTo-SecureString -AsPlainText "999999" -Force) -ChangePasswordAtLogin $True -Company "TekPerfek" -Title "idk" -State "California" -City "Oakland" -Description "just another rad dad" -EmployeeNumber "06"-Department "Executives" Displayname "Choncey Nunn" -Country "US" -PostalCode "01234" -Enabled $True
