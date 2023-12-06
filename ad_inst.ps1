b6ff685a9acea0c275633fe5080d94566d942bfd
choncey <nchoncey@gmail.com>
2023-12-05 17:30:41 -0800

    installing AD

--- ad_inst.ps1
+++ ad_inst.ps1
@@ -6,3 +6,5 @@ install-windowsfeature AD-Domain-Services
 
 # This will install Domain Server for AD
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "choncey.net" -DomainNetbiosName "CHONCEY" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+
+
