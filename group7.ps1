bd189b6613a6d004d8a0b346fdd7c13fa907b0b6
choncey <nchoncey@gmail.com>
2023-12-05 21:47:36 -0800

    creating group scripts

--- group7.ps1
+++ group7.ps1
@@ -1,2 +1,4 @@
 # This will install groups
 New-ADGroup -Name "IT" -Description "This is for IT" -GroupScope Universal -ManagedBy "ChonceyNunn"
+
+New-ADGroup -Name "Finance" -Description "This is for Finance" -GroupScope Universal -ManagedBy "MauriceBey"
