f7b05d46f6b7a49b1b4179821a1c656aceba531e
choncey <nchoncey@gmail.com>
2023-12-08 12:39:45 -0800

    updated script to create users/groups, and assign managers

--- ps_milestone2.ps1
+++ ps_milestone2.ps1
@@ -3,3 +3,8 @@ New-ADUser -Name "Choncey Nunn" -GivenName "Choncey" -Surname "Nunn" -SamAccount
 
 # This script will create new user: Maurice
 New-ADUser -Name "Maurice Bey" -GivenName "Maurice" -Surname "Bey" -SamAccountName "maurice" AccountPassword (ConvertTo-SecureString -AsPlainText "999999" -Force) -ChangePasswordAtLogin $True -Company "TekPerfek" -Title "idk" -State "California" -City "Oakland" -Description "just another rad dad" -EmployeeNumber "06"-Department "Executives" Displayname "Maurice Bey" -Country "US" -PostalCode "01234" -Enabled $Tru
+
+# This will create new groups (IT) (Finance), and add users to manage them
+New-ADGroup -Name "IT" -Description "This is for IT" -GroupScope Global -ManagedBy "ChonceyNunn"
+
+New-ADGroup -Name "Finance" -Description "This is for Finance" -GroupScope Global -ManagedBy "MauriceBey"
